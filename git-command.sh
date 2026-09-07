5ba7134f14fa16a86f0388d67d044a46e57a100e
yourUsername <yourEmail@example.com>
2026-09-07 02:18:34 -0500

    Add IRSA service account and update deployment.yaml manifest

--- git-command.sh
+++ git-command.sh
@@ -5,7 +5,7 @@ echo "Add files and do local commit"
 git add .
 echo "checking status"
 git status
-git commit -am "pushing gitops repos, kubernetes yaml manifests and helm charts"
+git commit -am "Add IRSA service account and update deployment.yaml manifest"
 
 echo "Pushing to Github Repository"
 git push -u origin master
